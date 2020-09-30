cd01b7348929cf3ed0ecd31570c899e79260ad9c
Carson Harter <carsonharter@gmail.com>
2020-09-30 09:42:57 -0400

    YuumaJr repaired/cleaned

--- DiscordBots/YuumaJr/main.js
+++ DiscordBots/YuumaJr/main.js
@@ -5,15 +5,10 @@ var token = new String('zU4NTAyODk0NTQ2MDU5Mjc0.X2v43g.0W_mElcdHTB0y-wsZClcQIHI8
 
 const prefix = '+';
 
-//initializing
-var code = new String('');//Among-Us Code Storage
-var sayOutput = new String('');//Storage for Say command
-var stalkerState = new Boolean();//Whether Stalker-Mode is on or off
-var stalkedUserID = new String('');//Multipurpose UserID storage for stalker
-
 const fs = require('fs');
 const { setPriority } = require('os');
-const { createInterface } = require('readline');
+//const { setPriority } = require('os');
+//const { createInterface } = require('readline');
 
 client.commands = new Discord.Collection();
 
@@ -24,95 +19,105 @@ for(const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
+var code = '';
+var sayOutput = '';
+var stalkerState = false;
+var stalkedUserID = '';
+
+/*                  Functions                       */
+
+function stalkerToggleStatement(message) {
+	if (!stalkerState) {
+		message.channel.send(':green_circle: Stalking Enabled :green_circle:');
+		message.channel.send('Now stalking: ' + stalkedUserID);
+	} else {
+		message.channel.send(':red_circle: Stalking Disabled :red_circle:');
+		message.channel.send('No longer stalking: ' + stalkedUserID);
+
+	}
+}
+
+function stalker(message) { //Assigns message ID to saved ID
+	if (stalkerState) {
+		if ((message.member).toString() != stalkedUserID) {
+			message.channel.send('I\'m sorry, you don\'t have permission to use this command at the moment');
+			return;
+		} /*else {
+			stalkerState = !stalkerState;
+		} */
+	} else {
+		stalkedUserID = (message.member).toString();
+	}
+
+	stalkerToggleStatement(message);
+	stalkerState = !stalkerState;
+}
+
+function stalkerChat(message) {
+	if ((message.member).toString() == stalkedUserID) {
+		message.channel.send('Yes! Yes! Say more!');
+	}
+}
+
+
+
+/* 				Beginning of Code					*/
+
 
 client.once('ready', () => {
-	console.log('Client Running');
-	stalkerState = false;
+	console.log('Client Online');
 });
 
 client.on('message', message => {
 	if(message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ + /);
-	const command = args.shift().toLowerCase();
-	
+	var command = args.shift().toLowerCase();
+
 	//commands to run after every message
-	client.commands.get('kinky').execute(message);
-
-	
-	if (stalkerState && stalkerUserID == (message.member).toString()) {
-		message.channel.send('Hello Friend');
-	}
-
-	
+	client.commands.get('kinky').execute(message);	
+	if (stalkerState) stalkerChat(message);
 	
 	//commands NOT requiring prefix
 	if (message.content.startsWith('@everyone') || message.content.startsWith('@here')) {
-
 		client.commands.get('snitch').execute(message, args);
-
 	}
 
 	if(!message.content.startsWith(prefix)) return; //tests for prefix
 
+	if (command.search(' ') != -1) {
+		var commandParam = command.slice(1 + command.search(' '));
+		command = command.slice(0, command.search(' '));
+	}
+
 	//commands requiring prefix
-	
-	if (command === 'commands'){
-		
-		message.channel.send(
-			">>> __**Available Commands:**__\n" +
-			"**Addcode [code]**: *Saves an Among-Us code to memory*\n" +
-			"**Code**: *Displays currently saved Among-Us code*\n" +
-			"**Ping**: *Pong!*\n" +
-			"**Say [message]**: *Yuuma replies with [message], deletes trigger message*\n" +
-			"**Stalker**: *Enables stalker function for user*\n\n" +
-
-			"__**Additional Functionalities:**__\n" +
-			"**Kinky**: *1% chance to reply \"Kinky\" to any message*\n" +
-			"**Snitch**: *Will rat anyone out who mass pings*"
-		);
-
-	} else if (command == 'log'){
-
-		client.commands.get('log').execute(message, args);
-
-	} else if (command == 'ping') {
-
-		client.commands.get('pong').execute(message, args);
-
-	} else if (command == 'stalker') {
-
-		
-
-		if (!stalkerState) {
-			stalkerUserID = message.member;
-			stalkerUserID = stalkerUserID.toString();
-		} else if (stalkerUserID != (message.member).toString()) {
-			message.channel.send('Im sorry, you do not have permission to do this');
-			return;
-		}
-
-		stalkerState = !stalkerState;
-		client.commands.get('stalker').execute(message, args, stalkerState, stalkerUserID);
-
-	} else if (command.startsWith('addcode')) {
-
-		code = command.slice(8, 20);
-		code = code.toUpperCase();
-		client.commands.get('addcode').execute(message, args, code);
-
-	} else if (command == 'code') {
-
-		message.channel.send(code);
-
-	} else if (command.startsWith('say')) {
-
-		client.commands.get('say').execute(message);
-
-	} else if (command.startsWith('qkzt')) {
-
-		client.commands.get('admin').execute(message, client);
-
+	switch (command) {
+		case 'commands':
+			client.commands.get('commandPrint').execute(message);
+		break;
+		case 'log':
+			client.commands.get('log').execute();
+		break;
+		case 'ping':
+			client.commands.get('pong').execute(message);
+		break;
+		case 'stalker':
+			stalker(message);
+		break;
+		case 'addcode':
+			client.commands.get('addcode').execute(message, commandParam);
+			code = commandParam;
+		break;
+		case 'code':
+			if (code != '') message.channel.send(code);
+			else message.channel.send('No code saved');
+		break;
+		case 'say':
+			client.commands.get('say').execute(message);
+		break;
+		case 'qkzt':
+			client.commands.get('admin').execute(message);
+		break;
 	}
 }); 
 
